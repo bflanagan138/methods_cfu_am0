e28129aa48ef9bfc724eb32a22d1c8633bad854d
Bryan Flanagan <bflanagan78@yahoo.com>
2022-08-04 13:09:16 -0600

    Adds define and id

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,17 @@
 
 # YOU DO the rest:
 
-
+#this method asks if the string object contains what is in the (). In this case does it contain "Hello" it returns `true`
 "Hello World".include?("Hello")
 
-
+#this method asks if the string object ends with Hello. It does not, so it returns `false`
 "Hello World".end_with?("Hello")
 
-
+#this method asks if string ends in rld. It does, so it returns `true`
 "Hello World".end_with?("rld")
 
-
+#this method asks if an integer is even. It is, so it returns `true`
 12.even?
 
-
+#this method prints the integer directly following the current integer. In this case it returns 19
 18.next
-
-
